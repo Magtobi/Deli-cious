3ec05a21ab87092598e95a6c0adc43308ba92dd4
rickeyglover <rickeygloverw@gmail.com>
2023-11-30 19:34:59 -0500

    completed receipt and checkout screen

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -32,9 +32,9 @@ public class Receipt {
 
             File file = new File(folder, fileName);
             BufferedWriter bufWriter = new BufferedWriter(new FileWriter(file));
+                bufWriter.write(date + " " + time);
 
             for (Order o : orders) {
-                bufWriter.write(date + " " + time);
                 bufWriter.newLine();
                 if (o instanceof SW) {
 
@@ -44,24 +44,37 @@ public class Receipt {
                 bufWriter.newLine();
                 bufWriter.write("Bread Type: " + o.getType());
                 bufWriter.newLine();
-               // bufWriter.write("Regular Toppings: ");
-               // for (String s : inputRegToppings) {
-               //     bufWriter.write(s + ",");
-                //}
-               // bufWriter.newLine();
+                bufWriter.write("Regular Toppings: ");
+                for (String s : inputRegToppings) {
+                    bufWriter.write(s + ", ");
                 }
+                bufWriter.newLine();
+                bufWriter.write("Premium Toppings: ");
+                    for (String s : inputPreToppings) {
+                        bufWriter.write(s + ", ");
+                    }
+                    bufWriter.newLine();
+                    bufWriter.write("Sauce: ");
+                    for (String s : userSauceList) {
+                        bufWriter.write(s + ", ");
+                    }
+                    bufWriter.newLine();
+                    bufWriter.write("Price: $" + o.getPrice());
+                    bufWriter.newLine();
+                }
+
              else if (o instanceof Drink){
                 bufWriter.write("Drink: " + o.getType());
                 bufWriter.newLine();
                 bufWriter.write("Size: " + o.getSize());
                 bufWriter.newLine();
-                bufWriter.write("Price: " + o.getPrice());
+                bufWriter.write("Price: $" + o.getPrice());
                 bufWriter.newLine();
              }
              else if(o instanceof Chips){
                  bufWriter.write("Chips Flavor: " + o.getType());
                  bufWriter.newLine();
-                 bufWriter.write("Price: " + o.getPrice());
+                 bufWriter.write("Price: $" + o.getPrice());
                  bufWriter.newLine();
                 }
                 //    bufWriter.newLine();
@@ -70,11 +83,20 @@ public class Receipt {
 
 
             bufWriter.newLine();
+            for (Order o : orders) {
+                totalCost += o.getPrice();
+            }
             bufWriter.write("Total Cost: $" + totalCost);
 
             bufWriter.close();
         } catch (IOException e) {
             System.out.println("Error writing receipt: " + e.getMessage());
         }
+
     }
+//    public static void totalCost () {
+//        for (Order o : orders) {
+//            totalCost += o.getPrice();
+//        }
+//    }
 }
